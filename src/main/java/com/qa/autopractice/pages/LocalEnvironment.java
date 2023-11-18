34c3b0619d808b792a930e69179a21ea011b8ec1
Akhil Chennuru <chennuruakhilrs19@gmail.com>
2023-11-18 16:29:08 +0530

    branch page update

---
+++ src/main/java/com/qa/autopractice/pages/LocalEnvironment.java
@@ -0,0 +1,10 @@
+package com.qa.autopractice.pages;
+
+public class LocalEnvironment {
+
+	public static void main(String[] args) {
+		System.out.println("Please look for the local environment");
+
+	}
+
+}
